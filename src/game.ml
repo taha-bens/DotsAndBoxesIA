4c6fbed90897309847ddd3f421ac2561d26260e0
Clément <clement.pinard04@gmail.com>
2024-10-16 22:44:33 +0200

    mise en place des 1e types et de la boucle principale

--- src/game.ml
+++ src/game.ml
@@ -1,3 +1,43 @@
 
-(* Une Partie *)
-let play_game () = print_string "Partie en cours \n"
+(* type (provisoire ?) *)
+type content =  Block |Content of string 
+
+type grid = string (*Array of array of content *)
+
+(* move = coordonnée * content *)
+type move = string * content
+
+(* Player = id * stratégie *)
+type player =  int * (grid -> move)
+
+(* La partie est elle terminée ? *)
+let game_complet () = Random.bool ()
+
+(* modification de la grille *)
+let make_wall () = ""
+
+(* mise à jour du score *)
+let update_score (l : int list) ((id,_) : player) = List.mapi (fun i x -> if i = id then x+1 else x) l
+
+(* Applique la stratégie d'un jouer sur la grille, renvoie true si l'action est autorisée *)
+let do_move ((id, strategie) : player) (g : grid) = print_string ("Le joueur : " ^ (string_of_int id) ^ " joue " ^ fst (strategie g) ^" sur la grille \n"); true
+
+
+(* Une Partie : Prend une liste de joueur et une grille de jeu et renvoie la liste des scores *)
+let play_game (players : player list) (g : grid) = 
+
+  let rec play_step score =
+    if not (game_complet ()) then
+
+      let score = List.fold_left 
+        (fun score player -> 
+        if do_move player g then 
+          update_score score player
+        else score)
+        score players in
+
+    play_step score 
+    else score
+  in 
+
+  play_step []
--- src/main.ml
+++ src/main.ml
@@ -1,17 +1,22 @@
 
+(* Variables d'essais *)
+let (g : Game.grid) = "grille"
+
+let (p : Game.player)  = (1,fun _ -> ("F1",Game.Block))
+
 
 (* Boucle principale du jeu *)
-let main_loop () = 
-  let continue = ref true in
-  while !continue do
-    Game.play_game ();
-    print_string "Voulez-vous jouer à nouveau ? (o/n) : ";
-    continue := match read_line () with
+let rec main_loop continue = 
+  if continue then
+    let _ = Game.play_game [p] g in
+    print_string "score \n";
+    print_string "Voulez-vous jouer à nouveau ? (o/n) : \n";
+    main_loop (match read_line () with
     | "o" | "O" | "oui" | "Oui" -> true
-    | _ -> false
-  done;
-  print_endline "Merci d'avoir joué ! \n"
+    | _ -> false)
+  else 
+  print_endline "Merci d'avoir joué ! \n" 
 
 
 (* Début du programme *)
-let () = main_loop ()
+let () = main_loop true
