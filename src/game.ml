f63dba2a7e4e842838408c26b1c6c28ccd44a3b5
Clément <clement.pinard04@gmail.com>
2024-10-20 17:49:25 +0200

    modification de types et gestion des différentes stratégies

--- src/game.ml
+++ src/game.ml
@@ -1,11 +1,10 @@
 
 (* type (provisoire ?) *)
-type content =  Block |Content of string 
 
 type grid = string (*Array of array of content *)
 
-(* move = coordonnée * content *)
-type move = string * content
+(* move = coordonnée * mur_pos *)
+type move = char * int * char
 
 (* Player = id * stratégie *)
 type player =  int * (grid -> move)
@@ -13,15 +12,13 @@ type player =  int * (grid -> move)
 (* La partie est elle terminée ? *)
 let game_complet () = Random.bool ()
 
-(* modification de la grille *)
-let make_wall () = ""
 
 (* mise à jour du score *)
 let update_score (l : int list) ((id,_) : player) = List.mapi (fun i x -> if i = id then x+1 else x) l
 
 (* Applique la stratégie d'un jouer sur la grille, renvoie true si l'action a permis de compléter une case *)
 (* Il faut vérifier que c'est bien un coup légale *)
-let do_move ((id, strategie) : player) (g : grid) = print_string ("Le joueur : " ^ (string_of_int id) ^ " joue " ^ fst (strategie g) ^" sur la grille \n"); true
+let do_move ((id, strategie) : player) (g : grid) = print_string ("Le joueur : " ^ (string_of_int id) ^ " joue " ^ (let (_,_,c) = strategie g in String.make 1 c) ^ " sur la grille \n"); true
 
 
 (* Une Partie : Prend une liste de joueurs et une grille de jeu et renvoie la liste des scores *)
--- src/player.ml
+++ src/player.ml
@@ -1,6 +1,33 @@
 
 (* Module des futures IA, C'est ici que le programme viens chercher la liste de joueurs *)
+
+
+(* stratégie d'un joueur sur le terminal*)
+let strategy_terminal = fun _ ->  let s = read_line () in (s.[0],int_of_char s.[1],s.[2]) (*faire des vérifications sur l'entrée*)
+
+(* stratégies :*)
+let strategyA = fun _ -> ('F',3,'N')  
+
+let strategyB = fun _ -> ('G',5,'O')
+
+
+
+let make_player_list (p : int) (botslist : string list) : Game.player list = 
+  List.init p (fun i -> (i + 1,strategy_terminal)) @
+
+  List.mapi 
+  (fun i s -> 
+    let offset = p + i + 1 in
+    match s with
+    | "sA" -> (offset,strategyA)
+    | "sB" -> (offset,strategyB)
+    | _ -> (offset,strategy_terminal)) botslist
+
+
+
 let (players : Game.player list) = [
-  (1,fun _ -> ("F1",Game.Block));
-  (2,fun _ -> ("C4", Game.Block))
+  (1,fun _ -> ('F',1,'E'));
+  (2,fun _ -> ('C',4,'S'));
+  (3, strategyA)
   ]
+
