d34f3750eccc07aab9a0bf282df8909c58567700
TewTeww <theo.salmona@gmail.com>
2024-10-30 19:46:52 +0100

    removing build directory after the execution

--- launcher.sh
+++ launcher.sh
@@ -3,11 +3,13 @@
 if [ $# -eq 0 ]; then
     dune build
     ./_build/default/bin/main.exe
+    rm -Rf _build
 elif [ $# -gt 1 ]; then
     echo "Erreur : exécuter './launcher.sh' ou './launcher.sh t'"
 elif [ "$1" == "t" ]; then
     dune build
-    dune runtest;
+    dune runtest
+    rm -Rf _build
 else
     echo "Erreur : exécuter './launcher.sh' ou './launcher.sh t'"
 fi
